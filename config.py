462ade1842f52562e2bade9107aa08d959718aab
kenielf <chrystian.keni.github@gmail.com>
2022-01-21 07:28:57 -0300

    Updated config.py to be usable

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 # General Config
-source_path = ""
-destination_path = ""
+source_path = ""  # Path to get images
+destination_path = source_path + "/convert"  # Path to convert images to, do not change this.
+autocopy = True  # Copy remaining files without prompting, leave "" if prompt is preferred, otherwise bool
 # Images
 img_types = [
     ".jpg",
     ".jpeg",
     ".webp"
 ]
-preferred_img_ext = ".png"
+p_img_ext = ".png"
 # Videos
 vid_types = [
     ".mkv",
     ".webm"
 ]
-preferred_vid_ext = ".mp4"
+p_vid_ext = ".mp4"
